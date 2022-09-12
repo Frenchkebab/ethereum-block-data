13d40c49f5584c52f0a368c2ad76179268fee334
frenchkebab <tubowangin@gmail.com>
2022-09-12 20:56:02 +0900

    [Header #3] Adds Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,54 @@
+import {
+  AppBar,
+  Container,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 import React from 'react';
 
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: 'gold',
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    height: '40',
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#fff',
+    },
+    type: 'dark',
+  },
+});
+
 const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+  const navigate = useNavigate();
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='transparent' position='static'>
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate('/')}
+              className={classes.title}
+              variant='h6'>
+              Home
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
 export default Header;
