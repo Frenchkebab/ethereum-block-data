672bf093c9f36a58514bb952c03b03fa450e5c10
frenchkebab <tubowangin@gmail.com>
2022-09-12 20:46:15 +0900

    Adds router and related basic component files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
 import HomePage from './Pages/HomePage';
+import Header from './Components/Header';
+import TransferVolume from './Components/Charts/TransferVolume/TransferVolume';
+import BaseFee from './Components/Charts/BaseFee/BaseFee';
+import GasRatio from './Components/Charts/GasRatio/GasRatio';
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: '#14161a',
+    color: 'white',
+    minHeight: '100vh',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className='App'>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/transferVolume' element={<TransferVolume />} />
+          <Route path='/baseFee' element={<BaseFee />} />
+          <Route path='/gasRatio' element={<GasRatio />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Charts/TransferVolume/TransferVolume.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TransferVolume = () => {
+  return <div>TransferVolume</div>;
+};
+
+export default TransferVolume;
